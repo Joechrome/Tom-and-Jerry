d59d4dd82d909c8b4ef447436a6b70e6253ac8e2
GAndyMo <doctorandymo2009@gmail.com>
2026-09-22 13:49:48 -0400

    I fixed the mid air infinite jump problem and allow for high and short jump
    High and short jump depends on hold time. Hold time, short, high jump's value are customizable.

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -1,31 +1,46 @@
+using Unity.Hierarchy;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float jumpForce = 10f;
     public Rigidbody2D playerRB;
+    private float airTime = 0f;
+    public float holdTime = 0.5f;
+    public float jumpForceShort = 2f;
+    public float jumpForceLong = 4f;
     bool isGrounded = false;
     void Start()
     {
         
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Check if the overlapping object is a collectible
-        if (other.CompareTag("floor"))
+        // Check if the colliding object is the floor
+        if (collision.gameObject.CompareTag("floor"))
         {
             isGrounded = true;
-            Debug.Log("Player is grounded");
         }
     }
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded == true)
+        if (Keyboard.current != null && Keyboard.current.spaceKey.isPressed && isGrounded == true)
         {
-            playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, jumpForce);
+            airTime += Time.deltaTime;
+        }
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasReleasedThisFrame && airTime <= holdTime && isGrounded == true)
+        {
+            playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, jumpForceShort);
+            isGrounded = false;
+            airTime = 0f;
+        }
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasReleasedThisFrame && airTime > holdTime && isGrounded == true)
+        {
+            playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, jumpForceLong);
+            isGrounded = false;
+            airTime = 0f;
         }
     }
 }
