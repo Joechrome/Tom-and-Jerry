3a036b1c50575ed6ac968c5a7b21604ec7fd3262
GAndyMo <doctorandymo2009@gmail.com>
2026-09-24 13:59:12 -0400

    I added text display on screen when reaching max jumping power.
    I used TMpro to acheive this

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -1,11 +1,13 @@
 using Unity.Hierarchy;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerJump : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Rigidbody2D playerRB;
+    public TextMeshProUGUI max;
     private float airTime = 0f;
     public float holdTime = 0.5f;
     public float jumpForceShort = 2f;
@@ -29,16 +31,22 @@ public class PlayerJump : MonoBehaviour
         if (Keyboard.current != null && Keyboard.current.spaceKey.isPressed && isGrounded == true)
         {
             airTime += Time.deltaTime;
+            if (airTime > holdTime)
+            {
+                max.text = "Max Power!";
+            }
         }
         if (Keyboard.current != null && Keyboard.current.spaceKey.wasReleasedThisFrame && airTime <= holdTime && isGrounded == true)
         {
             playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, jumpForceShort);
             isGrounded = false;
             airTime = 0f;
+            max.text = "";
         }
         if (Keyboard.current != null && Keyboard.current.spaceKey.wasReleasedThisFrame && airTime > holdTime && isGrounded == true)
         {
             playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, jumpForceLong);
+            max.text = "";
             isGrounded = false;
             airTime = 0f;
         }
